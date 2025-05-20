6d66975d5340ee126b7ed6c77f5087d3e3f37575
Nicolasprogam <lerschnicolas@gmail.com>
2025-05-20 19:11:05 -0300

    atv2inc.c

--- atv2inc.c
+++ atv2inc.c
@@ -2,32 +2,36 @@
 
 #include <stdio.h>
 
-int main()
-{
-    int num1,num2;
+int main() {
+    int num1, num2;
     char operacao;
-    printf("digita dois numeros: ");
-    scanf("%d %d", &num1 ,&num2);
-    printf("digite uma operaçao");fflush(stdin);
-    scanf("%c", &operacao); 
-    switch(operacao){
+
+    printf("Digite dois números: ");
+    scanf("%d %d", &num1, &num2);
+
+    printf("Digite uma operação : ");fflush(stdin);
+    scanf(" %c", &operacao); 
+
+    switch (operacao) {
         case '+':
-            printf("%d", num1 + num2);
+            printf("%d\n", num1 + num2);
             break;
         case '-':
-            printf("%d", num1 - num2);
+            printf("%d\n", num1 - num2);
             break;
         case '*':
-            printf("%d", num1 * num2);
+            printf("%d\n", num1 * num2);
             break;
         case '/':
-            printf("%d", num1 / num2);
+            if (num2 != 0) {
+                printf("%d\n", num1 / num2);
+            } else {
+                printf("Erro: divisão por zero.\n");
+            }
             break;
         default:
-            printf("operaçao invalida  ");
-        
-    
+            printf("Operação inválida.\n");
     }
-    
+
     return 0;
 }
